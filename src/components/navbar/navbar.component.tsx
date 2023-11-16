49b261e33fa1f5cdeaf35e14cf6d0cd3ab89f68d
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-16 17:39:24 -0300

    ajustando imports

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -3,7 +3,6 @@ import { NavbarMobile } from "./navbar-mobile";
 import { NavbarDesktop } from "./navbar-desktop";
 import { navbarString } from "./navbar-string";
 
-import logo from "../../assets/icon/logo.svg";
 
 export const Navbar = () => {
   const menuItems = [
@@ -17,7 +16,7 @@ export const Navbar = () => {
   return (
     <div className="flex flex-col items-center justify-around p-4 h-24 w-auto shadow-xl md:flex-row md:justify-around md:items-center md:px-32 md:h-20 ">
       <div className="flex gap-4 items-center">
-        <img src={logo} alt="logo" />
+        <img src="icon/logo.svg" alt="logo" />
         <h1 className="text-2xl">{navbarString.title}</h1>
       </div>
       <NavbarMobile menuItems={menuItems} />
