6f54d0fdb8511b5e114121d6eb7a12c1ff756d9a
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-16 12:52:08 -0300

    criando base do navbar

---
+++ src/components/navbar/navbar.component.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>Antonio Carlos</h1>
+      <ul>
+        <li>Home</li>
+        <li>sobre</li>
+        <li>Portifolio</li>
+        <li>Serviços</li>
+        <li>Resumo</li>
+        <li>Contato</li>
+      </ul>
+    </div>
+  );
+};
