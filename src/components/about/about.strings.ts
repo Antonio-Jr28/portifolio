5dc60c5f0d8dc36d78d69a00bea84701eaef0005
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-17 14:18:57 -0300

    adicionando foto

---
+++ src/components/about/about.strings.ts
@@ -0,0 +1,16 @@
+export const AboutStrings = {
+  title: "Antonio Carlos",
+  About:
+    "Meu nome é Antonio Carlos Gonçalves Junior 👋 Sou brasileiro, Suzano/SP. Tenhos dois filhos, busco uma quebra de ciclo para ter um futuro melhor e possibilitar uma nova vida para minha familia.",
+  subTitle: "Hard Skills",
+  hardSkills: {
+    react: "React",
+    typescript: "Typescript",
+    nodeJs: "Node.Js",
+    styledCompoonents: "Styled-Componentes",
+    tailWindCSS: "Tailwind CSS",
+    css: "CSS",
+    html: "HTML",
+    figma: "Figma",
+  },
+};
